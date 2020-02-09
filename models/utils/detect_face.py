b2cd3490031b4a75c857a713d26872756f25366c
Tim Esler <tim.esler@gmail.com>
2020-02-08 17:03:49 -0800

    Prevent memory accumulation in numpy NMS

--- models/utils/detect_face.py
+++ models/utils/detect_face.py
@@ -180,12 +180,14 @@ def generateBoundingBox(reg, probs, scale, thresh):
 def nms_numpy(boxes, scores, threshold, method):
     if boxes.size == 0:
         return np.empty((0, 3))
+
     x1 = boxes[:, 0]
     y1 = boxes[:, 1]
     x2 = boxes[:, 2]
     y2 = boxes[:, 3]
     s = scores
     area = (x2 - x1 + 1) * (y2 - y1 + 1)
+
     I = np.argsort(s)
     pick = np.zeros_like(s, dtype=np.int16)
     counter = 0
@@ -194,18 +196,26 @@ def nms_numpy(boxes, scores, threshold, method):
         pick[counter] = i
         counter += 1
         idx = I[0:-1]
+
         xx1 = np.maximum(x1[i], x1[idx])
         yy1 = np.maximum(y1[i], y1[idx])
         xx2 = np.minimum(x2[i], x2[idx])
         yy2 = np.minimum(y2[i], y2[idx])
+
         w = np.maximum(0.0, xx2 - xx1 + 1)
         h = np.maximum(0.0, yy2 - yy1 + 1)
+        del xx1, yy1, xx2, yy2
+
         inter = w * h
+        del w, h
+
         if method is "Min":
             o = inter / np.minimum(area[i], area[idx])
         else:
             o = inter / (area[i] + area[idx] - inter)
         I = I[np.where(o <= threshold)]
+        del o
+
     pick = pick[:counter]
     return pick
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools, os
 
 PACKAGE_NAME = 'facenet-pytorch'
-VERSION = '2.0.1'
+VERSION = '2.1.0'
 AUTHOR = 'Tim Esler'
 EMAIL = 'tim.esler@gmail.com'
 DESCRIPTION = 'Pretrained Pytorch face detection and recognition models'
