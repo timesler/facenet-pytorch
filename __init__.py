6ad818a079cc85ff3c4e0c5e035f1cda1808b2da
Aryan Soman <aryan.soman10@gmail.com>
2020-08-08 12:56:14 -0700

    Added warnings filter for .nonzero()

--- __init__.py
+++ __init__.py
@@ -2,3 +2,10 @@ from .models.inception_resnet_v1 import InceptionResnetV1
 from .models.mtcnn import MTCNN, PNet, RNet, ONet, prewhiten, fixed_image_standardization
 from .models.utils.detect_face import extract_face
 from .models.utils import training
+
+import warnings
+warnings.filterwarnings(
+    action="ignore", 
+    message="This overload of nonzero is deprecated:\n\tnonzero()", 
+    category=UserWarning
+)
