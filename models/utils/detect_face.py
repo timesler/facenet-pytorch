d78fcc801956786f3512f2a7fec11a2e2fc04093
Gao Ruijun <rckgao@gmail.com>
2021-04-27 14:03:03 +0800

    Fix wrong use of `is`

--- models/utils/detect_face.py
+++ models/utils/detect_face.py
@@ -247,7 +247,7 @@ def nms_numpy(boxes, scores, threshold, method):
         h = np.maximum(0.0, yy2 - yy1 + 1).copy()
 
         inter = w * h
-        if method is "Min":
+        if method == 'Min':
             o = inter / np.minimum(area[i], area[idx])
         else:
             o = inter / (area[i] + area[idx] - inter)
