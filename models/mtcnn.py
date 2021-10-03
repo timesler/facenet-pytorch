b2ca7f9053b87a7ae2a2e683521cc9ae789666a7
Dmitry Ioksha <41271764+DmitryIo@users.noreply.github.com>
2021-10-03 18:50:32 +0300

    Update mtcnn.py

--- models/mtcnn.py
+++ models/mtcnn.py
@@ -265,9 +265,9 @@ class MTCNN(nn.Module):
         faces = self.extract(img, batch_boxes, save_path)
 
         if return_prob:
-            return faces, batch_boxes.squeeze(), batch_probs.squeeze()
+            return faces, batch_boxes, batch_probs.squeeze()
         else:
-            return faces, batch_boxes.squeeze()
+            return faces, batch_boxes
 
     def detect(self, img, landmarks=False):
         """Detect all faces in PIL image and return bounding boxes and optional facial landmarks.
